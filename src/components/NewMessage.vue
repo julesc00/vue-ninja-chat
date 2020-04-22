6fcf3b499476008166c789c8c603bb23148e4bc1
juliobriones <julesc00@hotmail.com>
2020-04-22 06:10:29 -0500

    Connected to firebase.

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -2,21 +2,33 @@
   <form @submit.prevent="addMessage">
     <label for="new-message">Nuevo mensaje (enter para agregar)</label>
     <input type="text" id="new-message" v-model="newMessage">
+    <p class="red-text" v-if="feedback">{{ feedback }}</p>
   </form>
 </template>
 
 <script>
+  import db from "@/firebase/init"
+
   export default {
     name: "NewMessage",
     data () {
       return {
-        newMessage: null
+        newMessage: null,
+        feedback: null,
       }
     },
     props: ["name"],
     methods: {
       addMessage() {
-        console.log(this.newMessage, this.name, Date.now())
+        if (this.newMessage) {
+          db.collection("messages").add({
+            content: this.newMessage,
+            name: this.name,
+            timestamp: Date.now()
+          }).catch(err => console.log(err));
+        } else {
+          this.feedback = "Debe escribir un mensaje para envíar uno."
+        }
       }
     }
   }
